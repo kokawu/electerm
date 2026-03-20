8bb74804504d7cc911e43a4288d74730fd18dc39
root <root@kokawudebian185>
2026-03-20 11:07:21 +0800

    fix: disable auto publish during tagged Windows build

--- build/bin/build-win-nsis.js
+++ build/bin/build-win-nsis.js
@@ -7,12 +7,14 @@ const {
 
 async function main () {
   const pb = builder
+  const publishMode = process.env.ELECTRON_BUILDER_PUBLISH_MODE
+  const publishArg = publishMode ? ` --publish ${publishMode}` : ''
   echo('running build for win part nsis installer')
 
   echo('build nsis')
   rm('-rf', 'dist')
   writeSrc('win-x64-installer.exe')
-  await run(`${pb} --win nsis`)
+  await run(`${pb} --win nsis${publishArg}`)
 }
 
 main()
